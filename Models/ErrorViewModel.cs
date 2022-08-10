c068283033dabd05db6fdfbaf6a47541bbfc03db
nhuntington11 <nhuntington11@gmail.com>
2022-08-10 11:45:56 -0600

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Run_Map_Planner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
